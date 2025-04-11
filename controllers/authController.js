9ff165752245d5b7d0aaa69e4400e293b42d786c
Alfredo Carrasquel <alfredocrdev@gmail.com>
2025-04-11 14:52:01 -0400

    Se agrega metodo POST

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,37 +5,64 @@ const TWILIO_API_KEY_SID = process.env.TWILIO_API_KEY_SID;
 const TWILIO_API_KEY_SECRET = process.env.TWILIO_API_KEY_SECRET;
 
 const getToken = (req, res) => {
-  // Obtener los parámetros idUser e idRequest de la query
-  const { idUser, idRequest } = req.query;
+  if (req.method === 'POST') {
+    const { user_identity, room_name, create_conversation } = req.body;
 
-  // Validar que ambos parámetros estén presentes
-  if (!idUser){
-    return res.status(400).json({ error: 'idUser es requerido' });
+    if (!user_identity || !room_name) {
+      return res.status(400).json({
+        error: 'user_identity y room_name son requeridos',
+      });
+    }
+
+    const AccessToken = twilio.jwt.AccessToken;
+    const VideoGrant = AccessToken.VideoGrant;
+
+    const token = new AccessToken(
+      TWILIO_ACCOUNT_SID,
+      TWILIO_API_KEY_SID,
+      TWILIO_API_KEY_SECRET,
+      { identity: user_identity }
+    );
+
+    const videoGrant = new VideoGrant({
+      room: room_name,
+    });
+    token.addGrant(videoGrant);
+
+    res.json({
+      token: token.toJwt(),
+      room_type: 'peer-to-peer',
+    });
+  } else {
+    const { idUser, idRequest } = req.query;
+
+    if (!idUser || !idRequest) {
+      return res.status(400).json({
+        error: 'idUser e idRequest son requeridos',
+      });
+    }
+
+    const AccessToken = twilio.jwt.AccessToken;
+    const VideoGrant = AccessToken.VideoGrant;
+
+    const token = new AccessToken(
+      TWILIO_ACCOUNT_SID,
+      TWILIO_API_KEY_SID,
+      TWILIO_API_KEY_SECRET,
+      { identity: idUser }
+    );
+
+    const room = `room${idRequest}`;
+    const videoGrant = new VideoGrant({
+      room,
+    });
+    token.addGrant(videoGrant);
+
+    res.json({
+      token: token.toJwt(),
+      room,
+    });
   }
-  if (!idRequest){
-    return res.status(400).json({ error: 'idRequest es requerido' });
-  }
-
-  const AccessToken = twilio.jwt.AccessToken;
-  const VideoGrant = AccessToken.VideoGrant;
-
-  const token = new AccessToken(
-    TWILIO_ACCOUNT_SID,
-    TWILIO_API_KEY_SID,
-    TWILIO_API_KEY_SECRET,
-    { identity: idUser }
-  );
-
-  const room = `room${idRequest}`;
-  const videoGrant = new VideoGrant({
-    room,
-  });
-  token.addGrant(videoGrant);
-
-  res.json({
-    token: token.toJwt(),
-    room,
-  });
 };
 
 module.exports = { getToken };
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getToken } = require('../controllers/authController');
 
-// Definir la ruta GET /getToken
+// Definir la ruta GET y POST /getToken
 router.get('/getToken', getToken);
+router.post('/getToken', getToken);
 
 module.exports = router;
